04a0a793a22cb96ffab63b751b38c3912a3cebd6
aMandyDev <amanda89conceicao@gmail.com>
2024-02-15 19:09:29 -0300

    delete dist

--- dist/test/index.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe('Teste unitário do projeto fullstack chalenge', () => {
-    it('should test something', () => {
-        expect(true).toBe(true);
-    });
-});
-//# sourceMappingURL=index.test.js.map
